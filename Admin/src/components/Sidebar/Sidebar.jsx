99ed6595b091d02c0344f6f32008841289d3a6be
sarabsharat <sarabsharat.sb@gmail.com>
2025-09-18 10:28:01 +0300

    17-9

--- Admin/src/components/Sidebar/Sidebar.jsx
+++ Admin/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./Sidebar.css"
+import { Link } from 'react-router-dom'
+import { FaCartShopping, FaList } from "react-icons/fa6";
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        
+        <Link to="/addproduct"  style={{textDecoration:"none"}}>
+        <div className='sidebarItem'>
+           <FaCartShopping />
+           <span>Add Product</span>
+          </div>
+        </Link>
+        <Link to="/listproduct"  style={{textDecoration:"none"}}>
+        <div className='sidebarItem'>
+           <FaList />
+           <span>Products List</span>
+          </div>
+        </Link>
     </div>
   )
 }
--- Admin/src/pages/admin/admin.jsx
+++ Admin/src/pages/admin/admin.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Admin.css"
+import Sidebar from '../../components/Sidebar/Sidebar'
 
 export const Admin = () => {
   return (
     <div className='admin'>
-        
+        <Sidebar/>
     </div>
   )
 }
